2d3d1372a878c3ec271f46d1b6977d8f708f5359
Andrew Crouch <j.andrew.crouch@gmail.com>
2024-12-02 12:57:56 -0500

    solution for day 1 part 2

--- 1/test.js
+++ 1/test.js
@@ -1,6 +1,5 @@
 const input = require("./input.js")
 
-const descending = (a, b) => a - b
 
 const difference = (input) => {
 
@@ -12,6 +11,14 @@ const difference = (input) => {
     return leftList.reduce((acc, curr, index) => {
         return acc += Math.abs(curr - rightList[index])
     }, 0)
+}
+const descending = (a, b) => a - b
+
+const similarity = (input) => {
+    const { leftList, rightList } = getLists(input)
+
+    const occurrences = rightList.reduce((acc, curr) => acc.set(curr, (acc.get(curr) || 0) + 1), new Map())
+    return leftList.reduce((acc, curr) => { return acc += (occurrences.get(curr) || 0) * curr }, 0)
 }
 
 const getLists = (input) => {
@@ -46,7 +53,10 @@ describe('day 1', () => {
     test('part 1 real data', () => {
         expect(difference(input)).toEqual(2742123)
     })
-    // test('part 2 test data', () => {
-    //     expect(false).toEqual(true)
-    // })
+    test('part 2 test data', () => {
+        expect(similarity(testInput)).toEqual(31)
+    })
+    test('part 2 real data', () => {
+        expect(similarity(input)).toEqual(21328497)
+    })
 })
