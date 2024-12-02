2b3c7a7057cc188be1b4282bd2f93ae8b485e6bf
Andrew Crouch <j.andrew.crouch@gmail.com>
2024-12-02 17:38:28 -0500

    day 2 part two solution

--- 2/test.js
+++ 2/test.js
@@ -44,6 +44,29 @@ const safeRows = (data) => {
     }, 0)
 }
 
+const isSafeDampened = (numbers) => {
+    let safe = false
+    for (let i = 0; i < numbers.length; i++) {
+        const copy = [...numbers]
+        copy.splice(i, 1)
+        if (isSafe(copy) === true) {
+            safe = true
+            break
+        }
+    }
+    return safe
+}
+
+const dampenedSafeRows = (data) => {
+    const rows = parseData(data)
+    return rows.reduce((acc, curr) => {
+        if (isSafeDampened(curr)) {
+            acc++
+        }
+        return acc
+    }, 0)
+}
+
 describe('day 2 part 1', () => {
     test('safe case 1', () => {
         expect(isSafe([7, 6, 4, 2, 1])).toBeTruthy()
@@ -68,5 +91,29 @@ describe('day 2 part 1', () => {
     })
     test('real data', () => {
         expect(safeRows(input)).toEqual(246)
+    })
+})
+
+describe('day 2 part 2', () => {
+    test('safe case 1', () => {
+        expect(isSafeDampened([7, 6, 4, 2, 1])).toBeTruthy()
+    })
+    test('safe case 2', () => {
+        expect(isSafeDampened([1, 3, 6, 7, 9])).toBeTruthy()
+    })
+    test('unsafe case 1', () => {
+        expect(isSafeDampened([1, 2, 7, 8, 9])).toBeFalsy()
+    })
+    test('safe case 3', () => {
+        expect(isSafeDampened([8, 6, 4, 4, 1])).toBeTruthy()
+    })
+    test('safe case 4', () => {
+        expect(isSafeDampened([1, 3, 2, 4, 5])).toBeTruthy()
+    })
+    test('test data', () => {
+        expect(dampenedSafeRows(testInput)).toEqual(4)
+    })
+    test('real data', () => {
+        expect(dampenedSafeRows(input)).toEqual(318)
     })
 })
