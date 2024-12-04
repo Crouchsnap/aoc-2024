123f1951a3f11ee14b787d3050bc4b37d5e5b002
Andrew Crouch <j.andrew.crouch@gmail.com>
2024-12-04 16:44:21 -0500

    day 4 part 2 solution

--- 4/test.js
+++ 4/test.js
@@ -16,12 +16,34 @@ const parseData = (data) => {
     return rows.map((row) => Array.from(row))
 }
 
-let foundLetters = Array(10).fill(Array(10).fill('*'))
-
 const neigboringCoordinates = [{ x: 1, y: 1 }, { x: 1, y: 0 }, { x: 1, y: -1 }, { x: 0, y: -1 }, { x: 0, y: 1 }, { x: -1, y: -1 }, { x: -1, y: 0 }, { x: -1, y: 1 }]
+const topLeft = { x: 1, y: -1 }
+const topRight = { x: 1, y: 1 }
+const bottomLeft = { x: -1, y: -1 }
+const bottomRight = { x: -1, y: 1 }
 
 const validCoordinate = (length, coordinate) => {
     return !(coordinate > length - 1 || coordinate < 0)
+}
+
+const checkNeighborsPartTwo = (coordinates, data) => {
+    let count = 0
+    const topLeftToCheck = { x: coordinates.x + topLeft.x, y: coordinates.y + topLeft.y }
+    const topRightToCheck = { x: coordinates.x + topRight.x, y: coordinates.y + topRight.y }
+    const bottomLeftToCheck = { x: coordinates.x + bottomLeft.x, y: coordinates.y + bottomLeft.y }
+    const bottomRightToCheck = { x: coordinates.x + bottomRight.x, y: coordinates.y + bottomRight.y }
+
+    if ([topLeftToCheck, topRightToCheck, bottomLeftToCheck, bottomRightToCheck].every((element) => {
+        return validCoordinate(data.length, element.x) && validCoordinate(data.length, element.y)
+    })) {
+        const backString = data[topLeftToCheck.x][topLeftToCheck.y] + 'A' + data[bottomRightToCheck.x][bottomRightToCheck.y]
+        const forwardString = data[bottomLeftToCheck.x][bottomLeftToCheck.y] + 'A' + data[topRightToCheck.x][topRightToCheck.y]
+
+        if ((backString === 'MAS' || backString === 'SAM') && (forwardString === 'MAS' || forwardString === 'SAM')) {
+            count++
+        }
+    }
+    return count
 }
 
 const checkNeighbors = (coordinates, data) => {
@@ -63,6 +85,19 @@ const doIt = (data) => {
     return counter
 }
 
+const doItPart2 = (data) => {
+    let parsed = parseData(data)
+    let counter = 0
+    parsed.forEach((row, xIndex) => {
+        row.forEach((point, yIndex) => {
+            if (point === 'A') {
+                counter += checkNeighborsPartTwo({ x: xIndex, y: yIndex }, parsed)
+            }
+        })
+    });
+    return counter
+}
+
 
 describe('day 2 part 1', () => {
     test('parser works', () => {
@@ -82,5 +117,11 @@ describe('day 2 part 1', () => {
 
 describe('day 2 part 2', () => {
 
+    test('test data', () => {
+        expect(doItPart2(testInput)).toEqual(9)
+    })
 
+    test('real data', () => {
+        expect(doItPart2(input)).toEqual(1921)
+    })
 })
